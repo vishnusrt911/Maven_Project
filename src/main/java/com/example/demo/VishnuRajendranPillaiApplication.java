2887f1bb36bc6a2215a326fcb27baaccc7d28fc4
vishnu <vishnursrt@gmail.com>
2023-03-04 21:48:45 -0500

    "Adding Greeting message for maven project"

--- src/main/java/com/example/demo/VishnuRajendranPillaiApplication.java
+++ src/main/java/com/example/demo/VishnuRajendranPillaiApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import java.util.Calendar;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,7 +10,19 @@ public class VishnuRajendranPillaiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(VishnuRajendranPillaiApplication.class, args);
-		System.out.println("Welcome to COMP367");
-	}
+		
+		//Show Greeting
+		Calendar c = Calendar.getInstance();
+		int timeOfDay = c.get(Calendar.HOUR_OF_DAY);
 
+		if(timeOfDay >= 0 && timeOfDay < 12){
+			System.out.println("Good Morning Vishnu Welcome to COMP367");       
+		}else if(timeOfDay >= 12 && timeOfDay < 16){
+			System.out.println("Good Afternoon Vishnu Welcome to COMP367");
+		}else if(timeOfDay >= 16 && timeOfDay < 21){
+			System.out.println("Good Evening Vishnu Welcome to COMP367");
+		}else if(timeOfDay >= 21 && timeOfDay < 24){
+			System.out.println("Good Night Vishnu Welcome to COMP367");
+		}
+	}
 }
